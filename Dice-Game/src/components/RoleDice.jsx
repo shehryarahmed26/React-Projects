2afaed521edd54a8fb3c891513be56d6d039397f
Shehryar <shehryarahmed268@gmail.com>
2024-09-11 14:53:50 +0500

    'updation'

--- Dice-Game/src/components/RoleDice.jsx
+++ Dice-Game/src/components/RoleDice.jsx
@@ -9,7 +9,7 @@ const RoleDice = ({randomdice, setrandomdice, diceroll, score, setscore}) => {
 
     return <div className="container">
     <div className="flex flex-col w-fit mx-auto text-center justify-center mt-2">
-            <img onClick={diceroll}  className="w-[225px]" src={`../../public/dice_${randomdice}.png`} alt="dice-1" />
+            <img onClick={diceroll}  className="w-[225px]" src={`public/Images/dice_${randomdice}.png`} alt="dice-1" />
             <p className="font-bold text-xl mb-3">Click on Dice to roll</p>
             <button onClick={() => setscore(0)} className="mb-2 rules border py-2 font-bold reset rounded">Reset Score</button>
             <button onClick={() => setrulesdata(!rulesdata)} className="bg-black rulesbtn text-white hover:font-bold py-2 rounded my-3">{rulesdata ? 'Hidden Rules' : 'Show Rules'}</button>
