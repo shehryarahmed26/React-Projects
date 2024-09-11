be923098a1989f2cf856da705c12a3bd704f6105
Shehryar <shehryarahmed268@gmail.com>
2024-09-11 14:58:44 +0500

    'updation'

--- Dice-Game/src/components/Startgame.jsx
+++ Dice-Game/src/components/Startgame.jsx
@@ -1,6 +1,6 @@
 let Startgame = ({togglebtn}) => {
     return <div className="flex w-full h-screen justify-center items-center px-5">
-        <img className="w-[550px]" src="public/Images/Dices.png" alt="" />
+        <img className="w-[550px]" src="/Images/Dices.png" alt="" />
         <div className="content">
             <h1 className="text-7xl font-bold">Dice Game</h1>
             <button onClick={togglebtn} className="bg-black text-white text-1xl px-16 py-2 rounded float-end mt-8 play transition">Play Now</button>
